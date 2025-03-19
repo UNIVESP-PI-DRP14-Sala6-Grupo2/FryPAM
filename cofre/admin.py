660216d2bcd7d9b4c528f43b6c006d4a63db77ab
Alexandre Pavanello e Silva <apavanello@live.com>
2025-03-19 00:51:01 -0300

    add jazzmine

---
+++ cofre/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin, Group
+from .models import User, Tenant, IAMAccount, PasswordRequest
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'role', 'is_staff', 'is_superuser')
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': ('email', 'password')
+            }
+        ),
+        (
+            'Permissions',
+            {
+                'fields':
+                    ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+            }
+        ),
+        (
+            'Important dates',
+            {
+                'fields': ('last_login',)
+            }
+        ),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'role'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.unregister(Group)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Tenant)
+admin.site.register(IAMAccount)
+admin.site.register(PasswordRequest)
