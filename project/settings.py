f13ddb10f023cd9ee9f7f1167a9a2ef893885614
Alexandre Pavanello e Silva <apavanello@live.com>
2025-03-19 01:26:12 -0300

    add CSRF_TRUSTED_ORIGINS to settings.py

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-nd2nb_$&e7od6l!3vkhy@6(r5uehgw07@e1pn=ui495bkj0co*
 DEBUG = True
 
 ALLOWED_HOSTS = ["backend-dev.pavops.net", "localhost", "127.0.0.1"]
-
+CSRF_TRUSTED_ORIGINS = ["backend-dev.pavops.net", "localhost", "127.0.0.1"]
 
 # Application definition
 
